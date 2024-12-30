18d3ace4f86e803ef8b09b947cce9c782c672b71
mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-12-30 22:43:28 +0100

    Initial commit

---
+++ MineSweeper/MineSweeper/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace MineSweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.scoreBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.difficultyLevel = new System.Windows.Forms.ComboBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.widthBox = new System.Windows.Forms.TextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scoreBox
+            // 
+            this.scoreBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scoreBox.Location = new System.Drawing.Point(32, 27);
+            this.scoreBox.Name = "scoreBox";
+            this.scoreBox.ReadOnly = true;
+            this.scoreBox.Size = new System.Drawing.Size(64, 13);
+            this.scoreBox.TabIndex = 0;
+            this.scoreBox.Text = "Score: 0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(32, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(33, 13);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Time:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(63, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(33, 13);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(102, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(64, 13);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Flags: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Difficulty:";
+            // 
+            // difficultyLevel
+            // 
+            this.difficultyLevel.FormattingEnabled = true;
+            this.difficultyLevel.Items.AddRange(new object[] {
+            "Easy",
+            "Medium",
+            "Hard",
+            "Extreme"});
+            this.difficultyLevel.Location = new System.Drawing.Point(276, 24);
+            this.difficultyLevel.Name = "difficultyLevel";
+            this.difficultyLevel.Size = new System.Drawing.Size(131, 21);
+            this.difficultyLevel.TabIndex = 5;
+            this.difficultyLevel.Text = "Easy";
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(647, 22);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(118, 23);
+            this.playButton.TabIndex = 7;
+            this.playButton.Text = "PLAY";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Height:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Width:";
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(503, 24);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(24, 20);
+            this.heightBox.TabIndex = 10;
+            this.heightBox.Text = "15";
+            // 
+            // widthBox
+            // 
+            this.widthBox.Location = new System.Drawing.Point(503, 52);
+            this.widthBox.Name = "widthBox";
+            this.widthBox.Size = new System.Drawing.Size(24, 20);
+            this.widthBox.TabIndex = 11;
+            this.widthBox.Text = "10";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(647, 55);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(118, 23);
+            this.stopButton.TabIndex = 12;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 651);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.widthBox);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.difficultyLevel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.scoreBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pigliate Sta Mina";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox scoreBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox difficultyLevel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.TextBox widthBox;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ MineSweeper/MineSweeper/Form1.cs
@@ -0,0 +1,549 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace MineSweeper
+{
+    public partial class Form1 : Form
+    {
+        //setting timer values
+        int seconds = 0;
+        int minutes = 0;
+
+        //generate play field
+        Button[,] btn = new Button[41, 90];
+        int[,] btn_prop = new int[41, 90];
+        int[,] saved_btn_prop = new int[41, 90];
+        Point coord;
+
+        //Table aspect
+        int start_x, start_y;
+        int height, width;
+
+        //match status
+        bool firstPlay = true;
+        bool gameOver = false;
+
+        //Game variables
+        int mines;
+        int flag_value = 10;
+        int flags;
+        
+
+        //Button aspect
+        int buttonSize = 25;
+        int distance_between = 25;
+
+        //Coeficients for difficulty
+        double easyCoef = 0.1f;
+        double mediumCoef = 0.2f;
+        double hardCoef = 0.3f;
+        double extremeCoef = 0.5f;
+
+        //Points that are around
+        int[] dx8 = { 1, 0, -1, 0, 1, -1, -1, 1 };
+        int[] dy8 = { 0, 1, 0, -1, 1, -1, 1, -1 };
+
+        //
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void setButtonImage(int x, int y)
+        {
+            btn[x, y].Enabled = false;
+
+            if (gameOver && btn_prop[x, y] == flag_value)
+                btn_prop[x, y] = saved_btn_prop[x, y];
+
+            if (gameOver)
+                timer1.Stop();
+
+            switch (btn_prop[x, y])
+            {
+                case 0:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources.blank;
+                    EmptySpace(x, y);
+                    break;
+
+                case 1:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._1;
+                    break;
+
+                case 2:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._2;
+                    break;
+
+                case 3:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._3;
+                    break;
+
+                case 4:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._4;
+                    break;
+
+                case 5:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._5;
+                    break;
+
+                case 6:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._6;
+                    break;
+
+                case 7:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._7;
+                    break;
+
+                case 8:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources._8;
+                    break;
+
+                case -1:
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources.bmb;
+                    if (!gameOver)
+                        GameOver();
+                    break;
+            }
+
+            }
+
+        void Discover_Map()
+        {
+            for (int i = 1; i <= width; i++)
+                for (int j = 1; j <= height; j++)
+                    if (btn[i, j].Enabled == true)
+                    {
+                        setButtonImage(i, j);
+                    }
+        }
+
+        void GameOver()
+        {
+            gameOver = true;
+            Discover_Map();
+
+            List<SoundPlayer> startAudio = new List<SoundPlayer>();
+            SoundPlayer firstAudioStart = new SoundPlayer(MineSweeper.Properties.Resources.castillo_its_not_looking_good_bruv);
+            SoundPlayer secondAudioStart = new SoundPlayer(MineSweeper.Properties.Resources.assets_ti_devi_spaventare);
+            startAudio.Add(firstAudioStart);
+            startAudio.Add(secondAudioStart);
+
+            playsound(startAudio);
+
+            MessageBox.Show("Game Over !");
+            
+        }
+
+        int isPointOnMap(int x, int y)
+        {
+            if (x < 1 || x > width || y < 1 || y > height)
+                return 0;
+            return 1;
+        }
+
+        
+
+        void EmptySpace(int x, int y)
+        {
+            if (btn_prop[x, y] == 0)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    int cx = x + dx8[i];
+                    int cy = y + dy8[i];
+
+                    if (isPointOnMap(cx, cy) == 1)
+                        if (btn[cx, cy].Enabled == true && btn_prop[cx, cy] != -1 && !gameOver)
+                        {
+                            int flagCounter = flags;
+                            flagCounter--;
+                            scoreBox.Text = "Score: " + flagCounter.ToString();
+                            setButtonImage(cx, cy);
+                        }
+                }
+            }
+        }
+
+        void Check_FlagWin()
+        {
+            bool win = true;
+
+            for (int i = 1; i <= width; i++)
+                for (int j = 1; j <= height; j++)
+                    if (btn_prop[i, j] == -1)
+                        win = false;
+
+            if (win)
+            {
+                WinGame();
+            }
+        }
+
+
+        void WinGame()
+        {
+            gameOver = true;
+            Discover_Map();
+            //gameProgress.Value = 0;
+
+            List<SoundPlayer> startAudio = new List<SoundPlayer>();
+            SoundPlayer firstAudioStart = new SoundPlayer(MineSweeper.Properties.Resources.assets_yeah_boi);
+            SoundPlayer secondAudioStart = new SoundPlayer(MineSweeper.Properties.Resources.assets_questa_volta_no);
+            startAudio.Add(firstAudioStart);
+            startAudio.Add(secondAudioStart);
+
+            playsound(startAudio);
+
+            MessageBox.Show("Win !");
+            
+        }
+
+        void Check_ClickWin()
+        {
+            bool win = true;
+            for (int i = 1; i <= width; i++)
+                for (int j = 1; j <= height; j++)
+                    if (btn[i, j].Enabled == true && saved_btn_prop[i, j] != -1)
+                        win = false;
+
+            if (win)
+            {
+                WinGame();
+            }
+        }
+
+        private void OneClick(object sender, EventArgs e)
+        {
+            coord = ((Button)sender).Location;
+            int x = (coord.X - start_x) / buttonSize;
+            int y = (coord.Y - start_y) / buttonSize;
+
+            if (btn_prop[x, y] != flag_value)
+            {
+
+                ((Button)sender).Enabled = false;
+                ((Button)sender).Text = "";
+
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Stretch;
+
+                if (btn_prop[x, y] != -1 && !gameOver)
+                {
+                    //gameProgress.Value++;
+                    //scoreBox.Text = "Score: " + gameProgress.Value.ToString();
+                    Check_ClickWin();
+                }
+
+                setButtonImage(x, y);
+            }
+        }
+
+        int MinesAround(int x, int y)
+        {
+            int score = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                int cx = x + dx8[i];
+                int cy = y + dy8[i];
+
+                if (isPointOnMap(cx, cy) == 1 && btn_prop[cx, cy] == -1)
+                    score++;
+            }
+            return score;
+        }
+
+        void SetMapNumbers(int x, int y)
+        {
+            for (int i = 1; i <= x; i++)
+                for (int j = 1; j <= y; j++)
+                    if (btn_prop[i, j] != -1)
+                    {
+                        btn_prop[i, j] = MinesAround(i, j);
+                        //Debugging
+                        //btn[i, j].Text = btn_prop[i, j].ToString();
+                        saved_btn_prop[i, j] = MinesAround(i, j);
+                    }
+        }
+
+        private void RightClick(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Right)
+            {
+
+                coord = ((Button)sender).Location;
+                int x = (coord.X - start_x) / buttonSize;
+                int y = (coord.Y - start_y) / buttonSize;
+
+                if (btn_prop[x, y] != flag_value && flags > 0)
+                {
+                    btn[x, y].BackgroundImageLayout = ImageLayout.Stretch;
+                    btn[x, y].BackgroundImage = MineSweeper.Properties.Resources.flag;
+                    btn_prop[x, y] = flag_value;
+                    flags--;
+                    Check_FlagWin();
+                }
+                else
+                    if (btn_prop[x, y] == flag_value)
+                {
+                    btn_prop[x, y] = saved_btn_prop[x, y];
+                    btn[x, y].BackgroundImageLayout = ImageLayout.Stretch;
+                    btn[x, y].BackgroundImage = null;
+                    flags++;
+                }
+
+                textBox4.Text = "Flags: " + flags;
+            }
+        }
+
+        void CreateButtons(int x, int y)
+        {
+            for (int i = 1; i <= x; i++)
+                for (int j = 1; j <= y; j++)
+                {
+                    btn[i, j] = new Button();
+                    btn[i, j].SetBounds(i * buttonSize + start_x, j * buttonSize + start_y, distance_between, distance_between);
+                    btn[i, j].Click += new EventHandler(OneClick);
+                    btn[i, j].MouseUp += new MouseEventHandler(RightClick);
+                    btn_prop[i, j] = 0;
+                    saved_btn_prop[i, j] = 0;
+                    btn[i, j].TabStop = false;
+                    Controls.Add(btn[i, j]);
+                }
+        }
+
+        void GenerateMap(int x, int y, int mines)
+        {
+            Random rand = new Random();
+            List<int> coordx = new List<int>();
+            List<int> coordy = new List<int>();
+
+            while (mines > 0)
+            {
+                coordx.Clear();
+                coordy.Clear();
+
+                for (int i = 1; i <= x; i++)
+                    for (int j = 1; j <= y; j++)
+                        if (btn_prop[i, j] != -1)
+                        {
+                            coordx.Add(i);
+                            coordy.Add(j);
+                        }
+
+                int randNum = rand.Next(0, coordx.Count);
+                btn_prop[coordx[randNum], coordy[randNum]] = -1;
+                saved_btn_prop[coordx[randNum], coordy[randNum]] = -1;
+                mines--;
+            }
+        }
+
+        void StartGame()
+        {
+            switch (difficultyLevel.Text)
+            {
+                case "Easy":
+                    mines = (int)(height * width * easyCoef);
+                    break;
+
+                case "Medium":
+                    mines = (int)(height * width * mediumCoef);
+                    break;
+
+                case "Hard":
+                    mines = (int)(height * width * hardCoef);
+                    break;
+
+                case "Extreme":
+                    mines = (int)(height * width * extremeCoef);
+                    break;
+            }
+
+            flags = mines;
+            gameOver = false;
+
+            //gameProgress.Value = 0;
+            
+
+            timer1.Start();
+            seconds = 0;
+            minutes = 0;
+
+            textBox4.Text = "Flags: " + flags;
+            scoreBox.Text = "Score: " + 0;
+
+            if (firstPlay)
+                CreateButtons(width, height);
+
+            GenerateMap(width, height, mines);
+            SetMapNumbers(width, height);
+
+        }
+
+        void ResetGame(int x, int y)
+        {
+            for (int i = 1; i <= x; i++)
+                for (int j = 1; j <= y; j++)
+                {
+                    btn[i, j].Enabled = true;
+                    btn[i, j].Text = "";
+                    btn[i, j].BackgroundImage = null;
+                    btn_prop[i, j] = 0;
+                    saved_btn_prop[i, j] = 0;
+                }
+        }
+
+        void Warnings(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    MessageBox.Show("Empty Fields !");
+                    break;
+                case 2:
+                    MessageBox.Show("Wrong Input !");
+                    break;
+
+            }
+        }
+
+        bool hasLetters(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+                if (!Char.IsDigit(s, i))
+                    return true;
+
+            return false;
+        }
+
+        bool CorrectFields()
+        {
+            bool result = true;
+
+            if (heightBox.Text == "" || widthBox.Text == "" || difficultyLevel.Text == "")
+            {
+                Warnings(1);
+                result = false;
+            }
+            else
+            if (heightBox.Text != "" && widthBox.Text != "" && difficultyLevel.Text != "")
+            {
+                if (hasLetters(heightBox.Text) || hasLetters(widthBox.Text))
+                {
+                    Warnings(2);
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        void SetDimensions()
+        {
+            height = int.Parse(heightBox.Text);
+            width = int.Parse(widthBox.Text);
+
+            if (height > 25)
+                height = 25;
+            else
+                if (height < 5)
+                height = 5;
+
+            if (width > 40)
+                width = 40;
+            else
+                if (width < 5)
+                width = 5;
+
+            heightBox.Text = height.ToString();
+            widthBox.Text = width.ToString();
+
+        }
+
+        void TableMargins(int x, int y)
+        {
+            start_x = (this.Size.Width - (width + 2) * distance_between) / 2;
+            start_y = (this.Size.Height - (height + 2) * distance_between) / 2;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            heightBox.Enabled = true;
+            widthBox.Enabled = true;
+            GameOver();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+
+            if (CorrectFields())
+            {
+
+                SetDimensions();
+                TableMargins(height, width);
+
+                if (firstPlay)
+                {
+                    StartGame();
+                    firstPlay = false;
+
+                    widthBox.Enabled = false;
+                    heightBox.Enabled = false;
+                }
+                else
+                    if (!firstPlay)
+                {
+                    ResetGame(width, height);
+                    StartGame();
+                }
+                stopButton.Enabled = true;
+            }
+
+            List<SoundPlayer> startAudio = new List<SoundPlayer>();
+            SoundPlayer firstAudioStart = new SoundPlayer(MineSweeper.Properties.Resources.assets_la_guerra_piu_totale);
+            SoundPlayer secondAudioStart = new SoundPlayer(MineSweeper.Properties.Resources.assets_benson_ultimi);
+            startAudio.Add(firstAudioStart);
+            startAudio.Add(secondAudioStart);
+
+            playsound(startAudio);
+
+        }
+
+        //timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if (seconds == 60)
+            {
+                minutes++;
+                seconds = 0;
+            }
+
+            if (seconds < 10)
+            {
+                textBox3.Text = $"{minutes.ToString()}:0{seconds.ToString()}";
+            }
+            else
+            textBox3.Text = $"{minutes.ToString()}:{seconds.ToString()}";
+        }
+
+        private void playsound(List<SoundPlayer> filelist)
+        {
+            Random randomSound = new Random();
+            int listLength = filelist.Count();
+            int soundIndex = randomSound.Next(0, listLength);
+
+            filelist[soundIndex].Play();
+        }
+    }
+}
